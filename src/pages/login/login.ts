b65a8b2312809f286c3fc74e7f3e46975b963b80
Jose Manuel Guzman Guerrero <jmguzmangu@nh.inet>
2018-03-01 10:39:30 -0500

    Signed-off-by: Jose Manuel Guzman Guerrero <jmguzmangu@nh.inet>

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
 import { Login } from '../../model/Login';
 import { MapaUbicacionPage } from '../mapa-ubicacion/mapa-ubicacion';
@@ -20,17 +20,33 @@ export class LoginPage {
 
   login: Login = { usuario: '', password: '' };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toastCtrl: ToastController) {
   }
 
   onLogin(form: NgForm) {
-    if (form.valid && this.login.usuario=="manuel" && this.login.password=="manuel") {
+    if (form.valid && this.login.usuario == "manuel" && this.login.password == "manuel") {
       this.navCtrl.push(MapaUbicacionPage);
+    } else {
+      this.presentToast();
     }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Datos incorrectos, vuelva a intentarlo.',
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
